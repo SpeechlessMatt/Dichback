3e7bc0a5df5b699af8063a72949965e8dd02b8d8
SpeechlessMatt <115862375+SpeechlessMatt@users.noreply.github.com>
2024-12-13 16:23:51 +0800

    init

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='dichback',
+      version='0.0.1',
+      description='二分回溯算法，通过逻辑和-逻辑函数单元在有效数据集中查找符合逻辑单元判别条件的数据子集',
+      long_description=long_description,    #包的详细介绍，一般在README.md文件内
+      long_description_content_type="text/markdown",
+      url='https://github.com/SpeechlessMatt/UtMail',
+      author='Czy_4201b',
+      author_email='speechlessmatt@qq.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      install_requires=[
+          "loguru>=0.7.2",
+          "requests>=2.32.3"
+      ],
+      zip_safe=False)
