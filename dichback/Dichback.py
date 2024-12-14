20fcba0ad50f095f549e3b8e6b91c434dd93f1ba
SpeechlessMatt <115862375+SpeechlessMatt@users.noreply.github.com>
2024-12-14 14:21:04 +0800

    v0.0.5 fixed

--- dichback/Dichback.py
+++ dichback/Dichback.py
@@ -73,6 +73,8 @@ class AlgorithmSet:
                 max_num = (mark + min_num) // 2
                 # 只剩一个了
                 if min_num == max_num:
+                    if self.logic_unit(effective_list[min_num]):
+                        return effective_list[min_num]
                     raise AlgorithmListIneffectiveError("所传入的‘有效数组‘并非有效")
 
     def dichotomy_backtracking_algorithm(self, effective_list: list) -> list:
